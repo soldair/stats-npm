437c74bc70dc3ace368162824b93083eae19167a
Ryan Day <soldair@gmail.com>
2017-01-06 08:46:52 -0800

    renders request concurrency grouped my time interval

--- reader.js
+++ reader.js
@@ -5,37 +5,53 @@ var installations = {}
 var requests = new Map()
 
 var time = 0
-
+var intervals = {}
+var intervalms = 100
+var count = 0
 
 var prev
 process.stdin.pipe(split()).pipe(t2.obj(function(chunk,enc,cb){
 
   var o = JSON.parse(chunk)
-  console.log(o)
 
-  if(!time) {
-    time = o.start
-    console.log('start ',o.id)
+  var now = o.end||o.start
+
+  var interval = Math.floor(now/intervalms)
+  time = now;
+
+  if(!intervals[interval]) {
+    if(count++) render(interval,intervals)
+    intervals[interval] = {} 
   }
-
-  
 
   if(o.type === 'request-start'){
-    //requests.set(o.snpmid,o)
-    console.log('request start ',o.snpmid)
+    intervals[interval][o.snpmid] = 1
+
+    //console.log('request start ',o.snpmid)
   } else if(o.type === 'request'){
     //requests.delete(o.snpmid)
-    console.log('request end',o.snpmid)
+    //console.log('request end',o.snpmid)
   }
-  //render()
+  cb()
+},function(cb){
+  // render the buffered interval.
+  render(Math.floor(time/intervalms),intervals)
   cb()
 }))
 
 
-function render(){
-  //var line = 
-  for(request of requests) {
-    
-  }
+function render(interval, intervals){
+  var keys = Object.keys(intervals)
+
+  var first = keys[0]
+  var requests = intervals[first]
+  delete intervals[first]
+
+  var s = interval+'\t:';
+  Object.keys(requests).forEach(function(k){
+    //requests[k]
+    s += '|'
+  })
+  console.log(s)
 }
 
